d2c37548b07f3575b954983137ee8edd0de49fee
juligarji <jgarzonj@unal.edu.co>
2017-09-23 08:26:25 -0500

    instalacion de autenticacion facebook

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170907180900) do
+ActiveRecord::Schema.define(version: 20170923131031) do
 
   create_table "clients", force: :cascade do |t|
     t.string "user"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 20170907180900) do
     t.string "name"
     t.float "price"
     t.integer "duration"
-    t.string "description"
     t.boolean "state"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -77,9 +76,7 @@ ActiveRecord::Schema.define(version: 20170907180900) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "client_id"
     t.integer "entity_id"
-    t.index ["client_id"], name: "index_quotations_on_client_id"
     t.index ["entity_id"], name: "index_quotations_on_entity_id"
   end
 
@@ -103,6 +100,10 @@ ActiveRecord::Schema.define(version: 20170907180900) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "name"
+    t.text "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
