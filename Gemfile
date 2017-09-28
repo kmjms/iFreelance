39188f14908cff0c47d901318371d91a1253c09b
kmjms <joicarpinteronar@unal.edu.co>
2017-09-28 15:10:34 -0500

    manejo de admin con login facebook

--- Gemfile
+++ Gemfile
@@ -57,4 +57,7 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+gem 'omniauth'
+gem 'omniauth-facebook'
+
 gem 'bcrypt', platforms: :ruby
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   mount RailsAdmin::Engine => '/', as: 'rails_admin'
   resources :job_logs
   resources :jobs
