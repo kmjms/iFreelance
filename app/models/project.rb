e7807f778ff1e1e63d6c6bb9a63491ac2d3e4cb2
kmjms <joicarpinteronar@unal.edu.co>
2017-10-24 09:55:57 -0500

    nuevas vistas

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ApplicationRecord
   has_many :detail_invoices
   has_many :incomes
   has_many :expenses
-  belongs_to :freelance
-  belongs_to :state_project
-  belongs_to :type_project
+  #belongs_to :freelance
+  #belongs_to :state_project
+  #belongs_to :type_project
 end
