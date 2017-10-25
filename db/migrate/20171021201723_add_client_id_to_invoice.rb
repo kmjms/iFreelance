e1cc3fde202754e7dec52d1607038533d32f89b1
kmjms <joicarpinteronar@unal.edu.co>
2017-10-24 19:04:39 -0500

    cambios

--- db/migrate/20171021201723_add_client_id_to_invoice.rb
+++ db/migrate/20171021201723_add_client_id_to_invoice.rb
@@ -1,7 +1,7 @@
 class AddClientIdToInvoice < ActiveRecord::Migration[5.1]
   def change
     add_reference :invoices, :client, foreign_key: true
-    add_column :invoices, :client_id, :integer
-    add_index :invoices, :client_id
+    #add_column :invoices, :client_id, :integer
+
   end
 end
