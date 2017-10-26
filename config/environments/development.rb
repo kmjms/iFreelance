2ab60c0e4d86c8f3d62873c9155055b310430bcc
Andrey Herrera Silva <kaherreras@unal.edu.co>
2017-10-26 06:15:59 +0000

    config.assets.precompile = ['*.js', '*.css', '*.css.erb','*.png','*.jpg']

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,7 +8,9 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
-
+  
+  config.assets.precompile = ['*.js', '*.css', '*.css.erb','*.png','*.jpg']
+  
   # Show full error reports.
   config.consider_all_requests_local = true
 
